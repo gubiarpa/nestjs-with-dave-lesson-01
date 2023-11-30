60d0de91cab1be0ed09c903c9c9f7d31cb797afb
Billy Arredondo <billy.arredondo@pucp.pe>
2023-11-30 02:12:00 -0500

    Implement Methods on Users Service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,32 +1,33 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+
+    constructor(private readonly usersService: UsersService) { }
+
     @Get() // GET /users or /users?role=value
     findAll(@Query("role") role?: "Intern" | "Engineer" | "Admin") {
-        if (!role)
-            return [];
-
-        return [{ role }];
+        return this.usersService.findAll(role);
     }
 
     @Get(":id") // GET /users/:id
     findOne(@Param("id") id: string) {
-        return { id };
+        return this.usersService.findOne(+id);
     }
 
     @Post() // POST /users
-    create(@Body() user: {}) {
-        return user;
+    create(@Body() user: { name: string, email: string, role: "Intern" | "Engineer" | "Admin" }) {
+        return this.usersService.create(user);
     }
 
     @Put(":id") // PUT /users/:id
-    put(@Param("id") id: string, @Body() userUpdate: {}) {
-        return { id, ...userUpdate };
+    put(@Param("id") id: string, @Body() userUpdate: { name: string, email: string, role: "Intern" | "Engineer" | "Admin" }) {
+        return this.usersService.update(+id, userUpdate);
     }
 
     @Delete(":id") // GET /users/:id
     delete(@Param("id") id: string) {
-        return { id };
+        return this.usersService.delete(+id);
     }
 }

bab19e464bc888f069333644a7c2a8722cf3ecf9
Billy Arredondo <billy.arredondo@pucp.pe>
2023-11-30 00:44:50 -0500

    Add Method on Users Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get() // GET /users or /users?role=value
+    findAll(@Query("role") role?: "Intern" | "Engineer" | "Admin") {
+        if (!role)
+            return [];
+
+        return [{ role }];
+    }
+
+    @Get(":id") // GET /users/:id
+    findOne(@Param("id") id: string) {
+        return { id };
+    }
+
+    @Post() // POST /users
+    create(@Body() user: {}) {
+        return user;
+    }
+
+    @Put(":id") // PUT /users/:id
+    put(@Param("id") id: string, @Body() userUpdate: {}) {
+        return { id, ...userUpdate };
+    }
+
+    @Delete(":id") // GET /users/:id
+    delete(@Param("id") id: string) {
+        return { id };
+    }
+}
